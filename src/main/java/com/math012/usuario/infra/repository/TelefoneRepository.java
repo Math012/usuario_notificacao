cb124d75d7f43b74e2931191e40a789ad2c39593
Math012 <math111.0811@gmail.com>
2025-06-16 05:06:11 -0300

    Adicionadas as classes de segurança

---
+++ src/main/java/com/math012/usuario/infra/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.math012.usuario.infra.repository;
+
+import com.math012.usuario.infra.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
